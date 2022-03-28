134c9c4f07e68dde28ae9b7765bc5581deebbe34
Carlos Contreras <cdca12mx@gmail.com>
2022-03-28 16:52:05 -0700

    Se agrega clase embebida PK de ComprasProducto

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,18 @@
+package com.platzi.market.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private  Long idCompra;
+
+    @Column(name = "id_producto")
+    private  Long idProducto;
+}
