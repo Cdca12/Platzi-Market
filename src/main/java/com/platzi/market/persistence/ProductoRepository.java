896ca5be62dc0179775d2766e61ac1da78f6cfc1
Carlos Contreras <cdca12mx@gmail.com>
2022-03-31 14:14:45 -0700

    Se agregan metodos a Repository de Producto

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -12,4 +15,27 @@ public class ProductoRepository {
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    public List<Producto> getByCategoria(Long idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+
+
 }
