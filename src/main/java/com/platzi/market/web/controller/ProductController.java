8f369ede467bb13d097d6477b5e08b424cbc9bfd
Carlos Contreras <cdca12mx@gmail.com>
2022-04-02 02:06:01 -0700

    Se agregan endpoints en ProductController para exponer API

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -4,8 +4,7 @@ import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,23 +16,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(Long productId) {
+    @GetMapping("/{productId}")
+    public Optional<Product> getProduct(@PathVariable("productId") Long productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(Long categoryId) {
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") Long categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(Long productId) {
+    @DeleteMapping("/{productId}")
+    public boolean delete(@PathVariable("productId") Long productId) {
         try {
             productService.delete(productId);
             return true;
