b35a5f7be2dd76a45555529f42a34961bfc8b42c
Carlos Contreras <cdca12mx@gmail.com>
2022-04-05 00:49:20 -0600

    Se agrega autorizacion con JWT

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.platzi.market.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,5 +21,21 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, KEY)
                 .compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
     }
 }
