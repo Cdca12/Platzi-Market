369f6d72d5956f6341dadbc179d377ed7d96424e
Carlos Contreras <cdca12mx@gmail.com>
2022-04-06 02:08:56 -0600

    Se permite la visualizacio de Swagger públicamente sin tener que estar autenticados

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(platziUserDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web)throws Exception {
+        // Permitir URL de Swagger
+        web.ignoring().antMatchers("/v3/api-docs/**", "/configuration/ui",
+                "/swagger-resources/**", "/configuration/security",
+                "/swagger-ui/**", "/webjars/**");
     }
 
     @Override
